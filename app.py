318da407676153be21a57259581c504e267203d5
kiraryder <kmorgan22@mail.wou.edu>
2025-05-05 22:01:11 -0700

    Tests are passing locally

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, request
 
-
 app = Flask(__name__)
 
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+@app.route('/api/calcs/<number>', methods=["GET"])
+def calculate(number):
+    try:
+        number = int(number)
+
+        if number <= 0:
+            return (
+                '{"error": "Non-positive integer"}',
+                400,
+                {'Content-Type': 'application/json'}
+            )
+
+        dec = number - 1
+        hex_value = hex(number)
+
+        response_data = '{"dec": ' + str(dec) + ', "hex": "' + hex_value + '"}'
+
+        return (
+            response_data,
+            200,
+            {'Content-Type': 'application/json'}
+        )
+
+    except ValueError:
+        return (
+            '{"error": "Invalid integer input"}',
+            400,
+            {'Content-Type': 'application/json'}
+        )
+
+
